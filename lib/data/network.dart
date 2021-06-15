8a9840fdd56abb657528430b279be701d1b9b372
MG_dev <68521263+mugon-dev@users.noreply.github.com>
2021-06-15 23:45:35 +0900

    api 연동, model 추가

--- lib/data/network.dart
+++ lib/data/network.dart
@@ -4,10 +4,23 @@ import 'package:http/http.dart' as http;
 
 class Network {
   final String uri;
-  Network(this.uri);
+  final String uri2;
+  Network(this.uri, this.uri2);
 
   Future<dynamic> getJsonData() async {
     var url = Uri.parse(uri);
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      String jsonData = response.body;
+      var parsingData = jsonDecode(jsonData);
+      return parsingData;
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  Future<dynamic> getAirData() async {
+    var url = Uri.parse(uri2);
     http.Response response = await http.get(url);
     if (response.statusCode == 200) {
       String jsonData = response.body;
